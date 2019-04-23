444998ba49a44de4e0045d7e78f7ec553ae8930b
Phil Cherner <pcherner@mit.edu>
2019-04-23 13:44:46 -0700

    Clean up

--- scripts/connect-acr.sh
+++ scripts/connect-acr.sh
@@ -2,15 +2,9 @@
 set -eou pipefail
 
 # NOTE: this script should be ran from the root of the repository; the CWD should reflect this
-REGISTRY=upskillBedrockRegistry
-VERSION=latest
-CONTAINER=vott-container
-AZURE_SUB_NAME="PELITTLE TEAM - CSE DWR"
-AZURE_SUB_ID=d36d0808-a967-4f73-9fdc-32ea232fc81d
-
 echo "registry=${REGISTRY}"
 echo "versio=${VERSION}"
-echo "container=${CONTAINER}"
+echo "container=${CONTAINER_NAME}"
 echo "azure_sub_name=${AZURE_SUB_NAME}"
 echo "azure_sub_id=${AZURE_SUB_ID}"
 
